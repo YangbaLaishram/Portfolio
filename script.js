2a3fc0317348d5760c359febf62451c1b137e5f9
YangbaLaishram <120402780+YangbaLaishram@users.noreply.github.com>
2025-11-08 23:51:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Typing Effect
+const typingText = document.getElementById('typingText');
+const texts = [
+    "MERN Stack Developer",
+    "Full-Stack Developer",
+    "React.js Developer",
+    "Node.js Developer"
+];
+let textIndex = 0;
